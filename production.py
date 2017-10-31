0eda8d1abc51cf840bdfdb4f7474989bae114d3e
unknown <julian.winkel@hu-berlin.de>
2017-10-31 19:58:51 +0100

    reading and cleaning html using loop and regex, creating dictionary that counts occurence of words

---
+++ production.py
@@ -0,0 +1,155 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 31 17:57:07 2017
+
+@author: Julian
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 31 13:08:35 2017
+
+@author: Julian
+"""
+
+import requests, re
+from collections import Counter
+
+url = "http://teachingamericanhistory.org/library/document/what-to-the-slave-is-the-fourth-of-july/"
+rawhtml = requests.get(url)
+# print(rawhtml.text)
+
+# add spaces between elements
+#wordlist = ''.join(rawhtml.text)
+#print(wordlist)
+
+# make sure these are unique!
+textstart = rawhtml.text.find("Mr") - 1
+textend = rawhtml.text.find("Foner") 
+textsub = rawhtml.text[textstart:textend]
+
+
+text = list(textsub)
+text2 = text # a copy
+openbool = False # find and delete <...> combinations
+openbackslash = False # find and delete /...> combinations
+
+for i in range(len(text)):
+#for i in text:    
+#for i in range(0,50):
+    print("i = ", i)
+    
+    if text[i] == '<':
+        openbool = True
+        print("deleting ", text[i])  
+        text[i] = "" # delete
+        continue
+        
+        while openbool:         
+                   
+                    if text[i] == '>':
+                        print("deleting ", text[i])  
+                        text[i] = ""
+                        openbool = False
+                        break
+                    
+                    else:
+                        print("deleting ", text[i])  
+                        text[i] = ""
+                        continue # switch to next mark, first one is always open
+
+    if text[i] == '/':
+        openbackslash = True
+        print("deleting ", text[i])  
+        text[i] = "" # delete
+        continue
+        
+        while openbackslash:         
+                   
+                    if text[i] == '>':
+                        print("deleting ", text[i])  
+                        text[i] = ""
+                        openbackslash = False
+                        break
+                    
+                    else:
+                        print("deleting ", text[i])  
+                        text[i] = ""
+                        continue # switch to next mark, first one is always open
+
+
+    else:
+        print("keeping", text[i])
+        continue #i += 1     
+             
+
+
+s = "".join(text)   
+
+# \xa0em> and p>\np> and br >\n and thugsem> and em> and \xa0 still left
+#plocation = s.find("/p>")   
+expression = "(\\xa0em)|(p>\\np>)|(br >\\n)|(thugsem>)|(em>)|(\\xa0)" 
+cleantext = re.sub(expression, '', s)       
+
+
+# Count in dictionary
+dat = list(cleantext.split())
+dict = {}
+for i in range(len(dat)):
+    print(i)
+    word = dat[i]
+    dict[word] = dat.count(word)
+    continue
+
+
+
+
+
+
+
+
+dat = list(cleantext.split())
+dict = []
+for i in range(len(dat)):
+    print(i)
+    word = dat[i]
+    dict.append(dat.count(word))
+    continue
+
+
+dat = list(cleantext.split())
+dict = {}
+for i in range(len(dat)):
+    print(i)
+    word = dat[i]
+    dict[word] = dat.count(word)
+    continue
+
+
+
+# Count occurence of words 
+import re
+
+dict = {}
+sentence_list = ['This is a sentence', 'This is a sentence']
+for sentence in sentence_list:
+    for word in re.split('\s', sentence): # split with whitespace
+        try:
+            dict[word] += 1
+        except KeyError:
+            dict[word] = 1
+print(dict)
+
+# now for real
+dat = list(cleantext.split())
+
+dict = {}
+sentence_list = dat
+for dat in sentence_list:
+    for word in re.split('\s', sentence): # split with whitespace
+        try:
+            dict[word] += 1
+        except KeyError:
+            dict[word] = 1
+print(dict)
+
