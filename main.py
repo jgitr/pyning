d2fac62764d7f08c4e9a35b3fc37656cd8b448cf
J.W <julian.winkel@growney.de>
2017-11-02 13:43:10 +0100

    free run

--- main.py
+++ main.py
@@ -88,28 +88,58 @@ cleantext = cleantextCAP.lower()
 
 # Count in dictionary
 dat = list(cleantext.split())
-dict = {}
+dict1 = {}
 for i in range(len(dat)):
     print(i)
     word = dat[i]
-    dict[word] = dat.count(word)
+    dict1[word] = dat.count(word)
     continue
 
 # use either this 
-hierarchy = [(k, dict[k]) for k in sorted(dict, key=dict.get, reverse=True)]
+hierarchy = [(k, dict1[k]) for k in sorted(dict1, key=dict1.get, reverse=True)]
 
-limit = 20
-vals = [x[0] for x in hierarchy[0:limit]]
-idx = [x[1] for x in hierarchy[0:limit]]
-
-
-plt.bar(idx, vals)
+#limit = 20
+#vals = [x[0] for x in hierarchy[0:limit]]
+#idx = [x[1] for x in hierarchy[0:limit]]
+#plt.bar(idx, vals)
 
 # Or use Pandas Data Frame
-df = pandas.DataFrame(dict, index = [0])
+#df = pandas.DataFrame(dict, index = [0])
+#df.head(5)
 
 #df.plot()
 #df.hist()
+#highest = sorted(dict1, key=dict1.get, reverse=True)[:10]
+#limit = 5
+#vals = [x[0] for x in highest[0:limit]]
+#idx = [x[1] for x in highest[0:limit]]
+#plt.bar(idx, vals)
 
 
 
+
+#plt.bar(list(dict1.keys()), dict1.values(), color='g')
+#plt.show()
+
+
+
+# Resort in list
+d = dict1
+items = [(v, k) for k, v in d.items()]
+items.sort()
+items.reverse()   
+items = [(k, v) for v, k in items]
+allhighest = items[0:10]
+dd = dict(allhighest)
+dd.keys()
+wanted_keys = dd.keys() # The keys you want
+bigdict = dict1
+dictshow = dict((k, bigdict[k]) for k in wanted_keys if k in bigdict)
+
+plt.bar(list(dictshow.keys()), dictshow.values(), color='g')
+plt.show()
+
+# Works
+#test = dict.get("the", "of")
+#plt.bar(list(dict.keys()), dict.values(), color='g')
+#plt.show()
