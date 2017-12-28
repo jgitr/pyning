d227428441fc8f33f3c8ef7dcbd2063f5a2f89c9
unknown <julian.winkel@hu-berlin.de>
2017-12-29 00:05:37 +0100

    init trump speech analysis

---
+++ main_trump.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 28 23:27:50 2017
+
+@author: Julian
+"""
+
+# For Text Extraction
+import pandas as pd
+import bs4
+import urllib
+
+# Further Analysis
+import matplotlib.pyplot as plt
+import re
+from nltk.corpus import stopwords
+import os
+import pysentiment as ps
+
+# Define URL
+url = "http://www.cnn.com/2017/01/20/politics/trump-inaugural-address/"
+
+raw_html = urllib.request.urlopen(url)
+parsed_html = bs4.BeautifulSoup(raw_html, "lxml") # Choose lxml parser
+
+# type bs4.element.ResultSet
+text = parsed_html.find_all("div", class_ = "zn-body__paragraph")
+
+# Speech stored in text list
+textl = [] # init
+for i in text:
+    textl.append(i.get_text())
+
+cleantext = str(textl)
+    
+
+# Count and create dictionary
+dat = list(cleantext.split())
+dict1 = {}
+for i in range(len(dat)):
+    print(i)
+    word = dat[i]
+    dict1[word] = dat.count(word)
+    continue
+
+
+# Filter Stopwords
+keys = list(dict1)
+filtered_words = [word for word in keys if word not in stopwords.words('english')]
+dict2 = dict((k, dict1[k]) for k in filtered_words if k in filtered_words)
+
+
+
+# Resort in list
+# Reconvert to dictionary
+
+def valueSelection(dictionary, length, startindex = 0): # length is length of highest consecutive value vector
+    
+    # Test input
+    lengthDict = len(dictionary)
+    if length > lengthDict:
+        return print("length is longer than dictionary length");
+    else:
+        d = dictionary
+        items = [(v, k) for k, v in d.items()]
+        items.sort()
+        items.reverse()   
+        itemsOut = [(k, v) for v, k in items]
+    
+        highest = itemsOut[startindex:startindex + length]
+        dd = dict(highest)
+        wanted_keys = dd.keys()
+        dictshow = dict((k, d[k]) for k in wanted_keys if k in d)
+
+        return dictshow;
+    
+dictshow = valueSelection(dictionary = dict2, length = 7, startindex = 0)
+
+# Save dictionaries for wordcloud
+text_file = open("Output.txt", "w")
+text_file.write(str(cleantext))
+text_file.close()
+
+
+# Plot
+n = range(len(dictshow))
+plt.bar(n, dictshow.values(), align='center')
+plt.xticks(n, dictshow.keys())
+plt.title("Most frequent Words")
+plt.savefig("plot.png")
+
+# Overview
+overview =  valueSelection(dictionary = dict2, length = 1000, startindex = 0)
+nOverview = range(len(overview.keys()))
+plt.bar(nOverview, overview.values(), color = "g", tick_label = "")
+plt.title("Word Frequency Overview")
+plt.xticks([])
+plt.savefig("overview.png")
+
+
+# Sentiment Analysis
+hiv4 = ps.HIV4()
+tokens = hiv4.tokenize(cleantext)
+score = hiv4.get_score(tokens)
+print(score)
+
+
+# Polarity
+# Formula: (Positive - Negative)/(Positive + Negative)
+
+# Subjectivity
+# Formula: (Positive + Negative)/N
+
+
